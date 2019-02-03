ade91be8c3e10a1900a369d6cc75c4ae2dfbd474
Hayley Walters <hwalters.alt@gmail.com>
2019-02-03 00:23:40 -0800

    Added new Unit circle quiz file. This will be edited in the future

---
+++ UnitCircleQuiz.java
@@ -0,0 +1,8 @@
+import java.util.*; //so i can use scanner and random int
+public class UnitCircleQuiz{
+   private int totalQuestions = 20;
+   private int correct = 0;
+   private int incorrect = 0;
+   
+   private static Scanner console = new Scanner(System.in);
+}
